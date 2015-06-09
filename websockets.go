3b23e8b2e6f753a26c97d12fceb61133c94adfc0
Luke Evers <me@lukevers.com>
2015-06-09 18:36:08 -0400

    Broadcast to everyone currently active.

--- websockets.go
+++ websockets.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/gin-gonic/gin"
 	"fmt"
-	"net/http"
 )
 
 var ws = websocket.Upgrader{
@@ -12,31 +11,46 @@ var ws = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
-func handleWsRoute(c *gin.Context) {
-	wshandler(c.Writer, c.Request)
-}
+var connections []*websocket.Conn
 
-func wshandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := ws.Upgrade(w, r, nil)
+func handleWsRoute(c *gin.Context) {
+	// Upgrade the connection to a websocket connection
+	conn, err := ws.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
 		fmt.Println("Failed to upgrade websocket: %s", err)
 		return
 	}
 
+	// Get the session for the current connection
+	session, err := store.Get(c.Request, "user")
+	if err != nil {
+		fmt.Println("Error trying to get session: %s", err)
+	}
+
+	// Figure out who this user is
+	user := session.Values["username"].(string)
+	session.Values["conn_id"] = len(connections)
+
+	// Add the connection to the list
+	connections = append(connections, conn)
+
 	for {
 		_, msg, err := conn.ReadMessage()
 		if err != nil {
+			// The connection is being closed
+			i := session.Values["conn_id"].(int)
+			// Remove the connection from our slice of connections
+			connections = append(connections[:i], connections[i+1:]...)
+			// Now break out of the for loop since we're done
 			break
 		}
 
-		session, err := store.Get(r, "user")
-		if err != nil {
-			fmt.Println("Error trying to get session: %s", err)
+		// Broadcast to everyone currently.
+		for _, c := range connections {
+			c.WriteJSON(&Message{
+				Username: user,
+				Message: string(msg),
+			})
 		}
-
-		conn.WriteJSON(&Message{
-			Username: session.Values["username"].(string),
-			Message: string(msg),
-		})
 	}
 }
